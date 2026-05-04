7e41aaaeaa636ea2357a4f18c8122bae2b214355
Murat65536 <99989538+Murat65536@users.noreply.github.com>
2026-05-03 22:01:13 -0400

    .

--- src/main/java/murat/simv2/SimV2Client.java
+++ src/main/java/murat/simv2/SimV2Client.java
@@ -7,6 +7,5 @@ public class SimV2Client implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         PathRenderer.register();
-        PathPredictionController.register();
     }
 }
